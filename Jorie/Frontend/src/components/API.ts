762cc72ab433b0cc16d3dcfd42c4f47e6d29f619
Aidwise-Demo <demo@aidwise.ai>
2025-03-07 12:56:58 +0530

    changed base url

--- Jorie/Frontend/src/components/API.ts
+++ Jorie/Frontend/src/components/API.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const site = axios.create({
-  baseURL: "http://localhost:5000", // Adjust as needed
+  // baseURL: "http://localhost:5000", // Adjust as needed
+  baseURL: "https://jorie-front-end.onrender.com", // Adjust as needed
+
   headers: {
     "Content-Type": "application/json",
   },
